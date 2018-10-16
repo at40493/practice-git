66a84d36b3aa194ba64ff75e3d27f4fde27f3479
at40493 <at9247@gmail.com>
2018-10-16 16:50:09 +0800

    list.h     - Add list_del and list_for_each_safe functions. homework.c - Test delete function by list_for_each_safe.

--- homework.c
+++ homework.c
@@ -14,19 +14,35 @@ int main()
 
 	LIST_HEAD(name);
 	struct Linked_list *tmp ;
-	struct list_head *iterator;
+	struct list_head *iterator, *temp;
+	int number = 0 ;
 	
 	for ( int i = -5 ; i < 10 ; i++){
 		
 		tmp = malloc(sizeof(struct Linked_list));
 		tmp -> num = i ;
 		list_add(&tmp->list,&name);
+		
 	}
+	
+
+	list_for_each_safe(iterator, temp ,&name){
+
+                number = list_entry(iterator, struct Linked_list, list)->num;
+
+                if(number == 2)
+		{
+			list_del(&list_entry(iterator, struct Linked_list, list)->list);
+			free(list_entry(iterator, struct Linked_list, list));			
+		}
+        }
 
 	list_for_each(iterator, &name){
 		
+		
 		printf("%d \n", list_entry(iterator, struct Linked_list, list)->num );
 	}
- return 0 ;
+
+	 return 0 ;
 
 }
--- list.h
+++ list.h
@@ -4,6 +4,33 @@
 #define LIST_HEAD_INIT(name) { &(name), &(name) }
 #define LIST_HEAD(name) \ 
         struct list_head name = LIST_HEAD_INIT(name)
+
+
+#ifdef CONFIG_ILLEGAL_POINTER_VALUE
+# define POISON_POINTER_DELTA _AC(CONFIG_ILLEGAL_POINTER_VALUE, UL)
+#else
+# define POISON_POINTER_DELTA 0
+#endif
+
+/*
+ * These are non-NULL pointers that will result in page faults
+ * under normal circumstances, used to verify that nobody uses
+ * non-initialized list entries.
+ */
+
+#define LIST_POISON1  ((void *) 0x00100100 + POISON_POINTER_DELTA)
+#define LIST_POISON2  ((void *) 0x00200200 + POISON_POINTER_DELTA)
+
+/**
+ * list_for_each_safe - iterate over a list safe against removal of list entry
+ * @pos:	the &struct list_head to use as a loop cursor.
+ * @n:		another &struct list_head to use as temporary storage
+ * @head:	the head for your list.
+ */
+#define list_for_each_safe(pos, n, head) \
+	for (pos = (head)->next, n = pos->next; pos != (head); \
+		pos = n, n = pos->next)
+
 #define list_for_each(pos, head) \
         for (pos = (head)->next; pos != (head); pos = pos->next)
 
@@ -13,6 +40,7 @@
  * @type:       the type of the struct this is embedded in.
  * @member:     the name of the list_head within the struct.
  */
+
 #define list_entry(ptr, type, member) \
         container_of(ptr, type, member)
 #define offsetof(TYPE, MEMBER) ((unsigned int) &((TYPE *)0)->MEMBER)
@@ -60,6 +88,28 @@ static inline void __list_del (struct list_head * prev, struct list_head * next)
 {
         next->prev = prev ;
         prev->next = next ;
+}
+
+/**
+ * list_del - deletes entry from list.
+ * @entry: the element to delete from the list.
+ * Note: list_empty() on entry does not return true after this, the entry is
+ * in an undefined state.
+ */
+
+static inline void __list_del_entry(struct list_head *entry)
+{
+	//if (!__list_del_entry_valid(entry))
+	//	return;
+
+	__list_del(entry->prev, entry->next);
+}
+
+static inline void list_del(struct list_head *entry)
+{
+	__list_del_entry(entry);
+	entry->next = LIST_POISON1;
+	entry->prev = LIST_POISON2;
 }
 
 // Check empty
