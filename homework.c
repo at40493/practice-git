62a1400a1f5f5c3a2e26ff1bd6643b7102a1b00d
at40493 <at9247@gmail.com>
2018-10-19 14:48:32 +0800

    Add openfile and the progarm can "add" and "delete" string.

--- homework.c
+++ homework.c
@@ -1,48 +1,102 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<list.h>
+#include<string.h>
+#include<errno.h>
+#include<limits.h>
+#include<assert.h>
+#include"list.h"
 
 
-struct Linked_list{
+struct Linked_list {
 
-	int num ; 
+	char str[500] ;
 	struct list_head list;
 };
 
 int main()
 {
 
-	LIST_HEAD(name);
-	struct Linked_list *tmp ;
-	struct list_head *iterator, *temp;
-	int number = 0 ;
-	
-	for ( int i = -5 ; i < 10 ; i++){
-		
-		tmp = malloc(sizeof(struct Linked_list));
-		tmp -> num = i ;
-		list_add(&tmp->list,&name);
-		
+	LIST_HEAD(head); // Create the list head.
+	struct Linked_list *link_list ;
+	struct list_head *list_head_iterator, *list_head_temp;
+	char str[128], *str_token, emptyStr = '\0';
+	char *saveptr1, *endptr;
+	FILE *fptr ;
+
+	// Check file exist.
+	if((fptr = fopen("input.txt", "r")) == NULL){
+		printf("open_file_error");
+		exit(1);
 	}
-	
 
-	list_for_each_safe(iterator, temp ,&name){
+	errno = 0 ; /* To distinguish success/failure after call */
+	while(fgets(str, 128, fptr) != NULL) {	// Scan the intput file
 
-                number = list_entry(iterator, struct Linked_list, list)->num;
+		str_token = strtok_r(str, " \n", &saveptr1); // Split the string.
 
-                if(number == 2)
-		{
-			list_del(&list_entry(iterator, struct Linked_list, list)->list);
-			free(list_entry(iterator, struct Linked_list, list));			
+		if(str_token == NULL) // Check pch is NULL or not.
+			str_token = &emptyStr ; // emptyStr = '\0'
+
+		if(!strcmp(str_token, "add")) { // Add
+			str_token = strtok_r(NULL, " \n", &saveptr1); // Split the string.
+			link_list = malloc(sizeof(struct Linked_list));
+			if(link_list) { // tmp value not NULL
+				strncpy(link_list->str, str_token, strlen(str_token) + 1); // Convert char *pch to char str[]
+
+				//tmp -> str = pch ;
+				list_add_tail(&link_list->list, &head);
+			} else { // tmp value is NULL.
+				printf("malloc error \n");
+				exit(1);
+
+			}
+		} else if(!strcmp(str_token, "del")) {
+			str_token = strtok_r(NULL, " \n", &saveptr1); // Split the str.
+
+			list_for_each_safe(list_head_iterator, list_head_temp , &head) {
+
+				//number = strtol(pch, &endptr, 10);
+
+				/* Check for various possible errors */
+				/*
+				if ((errno == ERANGE && (number == LONG_MAX || number == LONG_MIN))
+				  || (errno != 0 && number == 0)) {
+				perror("strtol");
+				exit(EXIT_FAILURE);
+				}
+
+				if (endptr == pch) {
+				fprintf(stderr, "No digits were found\n");
+				exit(EXIT_FAILURE);
+				}
+				*/
+				link_list = list_entry(list_head_iterator, struct Linked_list, list);
+
+				// Find the string which wants to delete.
+				if(!strcmp(link_list->str, str_token)) {
+					list_del(&link_list->list);
+					free(link_list); // Free the memory
+				}
+			}
+		} else {
+			//printf("------ \n");
+			//printf("%s \n",pch);
 		}
-        }
-
-	list_for_each(iterator, &name){
-		
-		
-		printf("%d \n", list_entry(iterator, struct Linked_list, list)->num );
 	}
 
-	 return 0 ;
+
+	fclose(fptr); // Close file.
+
+	printf("Print result : \n");
+	list_for_each(list_head_iterator, &head) {
+
+		link_list = list_entry(list_head_iterator, struct Linked_list, list);
+		printf("%s \n", link_list->str);
+		free(link_list); // Free the memory.
+	}
+
+
+
+	return 0 ;
 
 }
