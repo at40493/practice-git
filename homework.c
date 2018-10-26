5cf394d1c1f45bbe81bce244e7f943229130bfc2
at40493 <at9247@gmail.com>
2018-10-26 11:37:35 +0800

    1.Modify parameter name. 2.Replace strcpy and malloc with strdup. 3.Remove exit(1)

--- homework.c
+++ homework.c
@@ -8,7 +8,7 @@
 void freememory(char*, struct list_head*);
 
 
-struct linked_list {
+struct linked_list_node {
 	char *str ;
 	struct list_head list;
 };
@@ -17,13 +17,12 @@ int main(int argc, char *argv[])
 {
 
 	LIST_HEAD(head); // Create the list head.
-	struct linked_list *link_list_new, *link_list_match;
+	struct linked_list_node *link_list_new_node, *link_list_match_node;
 	struct list_head *list_head_iterator, *list_head_temp;
 	char *str = NULL, *str_token_operator, *str_token_match, *str_token_str, *str_token_next;
 	size_t len = 0;
 
 	while(getline(&str, &len, stdin) != -1) {	// Scan the intput file
-		
 		str_token_operator = strtok_r(str, " \n", &str_token_next); // Split the string.
 
 		if(str_token_operator == NULL) // Check str_token is NULL or not.
@@ -37,22 +36,23 @@ int main(int argc, char *argv[])
 				fprintf(stderr, "Incorrect add format\n");
 				continue;
 			}
-			link_list_new = malloc(sizeof(struct linked_list));
-			if(link_list_new == NULL) {
-				fprintf(stderr, "Malloc error from link_list_new \n");
+			link_list_new_node = malloc(sizeof(struct linked_list_node));
+			if(link_list_new_node == NULL) {
+				fprintf(stderr, "Malloc error from link_list_new_node \n");
 				freememory(str, &head);
+				return 0 ;
 			}
 
-			link_list_new->str = malloc((strlen(str_token_str) + 1) * sizeof(char *));
-			if(link_list_new->str == NULL) { // link list  NULL
-				fprintf(stderr, "Malloc error from string : %s \n", link_list_new->str);
-				free(link_list_new);
+			link_list_new_node->str = strdup(str_token_str);
+			if(link_list_new_node->str == NULL) { // link list  NULL
+				fprintf(stderr, "Malloc error from link_list_new_node->str \n");
+				free(link_list_new_node);
 				freememory(str, &head);
-
+				return 0 ;
 			}
-			strcpy(link_list_new->str, str_token_str); // Copy string
 
-			list_add_tail(&link_list_new->list, &head);
+
+			list_add_tail(&link_list_new_node->list, &head);
 
 
 		} else if(strcmp(str_token_operator, "del") == 0) {
@@ -63,14 +63,13 @@ int main(int argc, char *argv[])
 				continue;
 			}
 			list_for_each_safe(list_head_iterator, list_head_temp , &head) {
-
-				link_list_match = list_entry(list_head_iterator, struct linked_list, list);
+				link_list_match_node = list_entry(list_head_iterator, struct linked_list_node, list);
 
 				// Find the string which wants to delete.
-				if(strcmp(link_list_match->str, str_token_str) == 0) {
-					list_del(&link_list_match->list);
-					free(link_list_match->str); // Free the memory.
-					free(link_list_match); // Free the memory
+				if(strcmp(link_list_match_node->str, str_token_str) == 0) {
+					list_del(&link_list_match_node->list);
+					free(link_list_match_node->str); // Free the memory.
+					free(link_list_match_node); // Free the memory
 
 					break;
 				}
@@ -86,27 +85,27 @@ int main(int argc, char *argv[])
 				continue;
 			}
 			list_for_each(list_head_iterator, &head) {
+				link_list_match_node = list_entry(list_head_iterator, struct linked_list_node, list);
 
-				link_list_match = list_entry(list_head_iterator, struct linked_list, list);
-
-				if(strcmp(str_token_match, link_list_match->str) == 0) {
-
-					link_list_new = malloc(sizeof(struct linked_list));
-					if(link_list_new == NULL) {
-						fprintf(stderr, "Malloc error from link_list_new \n");
+				if(strcmp(str_token_match, link_list_match_node->str) == 0) {
+					link_list_new_node = malloc(sizeof(struct linked_list_node));
+					if(link_list_new_node == NULL) {
+						fprintf(stderr, "Malloc error from link_list_new_node \n");
 						freememory(str, &head);
+						return 0 ;
 					}
 
-					link_list_new->str = malloc((strlen(str_token_str) + 1) * sizeof(char *));
-					if(link_list_new->str == NULL) {
-						fprintf(stderr, "Malloc error from string : %s \n", link_list_new->str);
-						free(link_list_new);
+					link_list_new_node->str = strdup(str_token_str);
+					if(link_list_new_node->str == NULL) {
+						fprintf(stderr, "Malloc error from link_list_new_node->str \n");
+						free(link_list_new_node);
 						freememory(str, &head);
+						return 0 ;
 					}
 
-					strcpy(link_list_new->str, str_token_str); // Copy string
 
-					list_add_tail(&link_list_new->list, &link_list_match->list); // list_add_tail(new,head)
+
+					list_add_tail(&link_list_new_node->list, &link_list_match_node->list); // list_add_tail(new,head)
 					break;
 
 				}
@@ -127,29 +126,31 @@ int main(int argc, char *argv[])
 			//fprintf(stderr, "1 str_token = %s\n", str_token_next);
 
 			list_for_each(list_head_iterator, &head) {
+				link_list_match_node = list_entry(list_head_iterator, struct linked_list_node, list);
 
-				link_list_match = list_entry(list_head_iterator, struct linked_list, list);
-
-				if(strcmp(str_token_match, link_list_match->str) == 0) { // Find the match.
+				if(strcmp(str_token_match, link_list_match_node->str) == 0) { // Find the match.
 
 					//fprintf(stderr, "2 str_token = %08x\n", str_token);
 					//fprintf(stderr, "2 str_token = %s\n", str_token);
 
-					link_list_new = malloc(sizeof(struct linked_list));
-					if(link_list_new == NULL) {
-						fprintf(stderr, "Malloc error from link_list_new \n");
+					link_list_new_node = malloc(sizeof(struct linked_list_node));
+					if(link_list_new_node == NULL) {
+						fprintf(stderr, "Malloc error from link_list_new_node \n");
 						freememory(str, &head);
+						return 0 ;
 					}
 
-					link_list_new->str = malloc((strlen(str_token_str) + 1) * sizeof(char *));
-					if(link_list_new->str == NULL) {
-						fprintf(stderr, "Malloc error from string : %s \n", link_list_new->str);
-						free(link_list_new);
-						freememory(str, &head);
-					}
-					strcpy(link_list_new->str, str_token_str); // Copy string
+					link_list_new_node->str = strdup(str_token_str);
 
-					list_add(&link_list_new->list, &link_list_match->list); // list_add(new,head)
+					if(link_list_new_node->str == NULL) {
+						fprintf(stderr, "Malloc error from link_list_new_node->str \n");
+						free(link_list_new_node);
+						freememory(str, &head);
+						return 0 ;
+					}
+
+
+					list_add(&link_list_new_node->list, &link_list_match_node->list); // list_add(new,head)
 					break;
 
 				}
@@ -162,14 +163,13 @@ int main(int argc, char *argv[])
 	free(str); // Free the memory.
 
 	fprintf(stderr, "Output file : output.txt \n");
-	list_for_each(list_head_iterator, &head) {
+	list_for_each_safe(list_head_iterator, list_head_temp, &head) {
+		link_list_new_node = list_entry(list_head_iterator, struct linked_list_node, list);
+		fprintf(stderr, "%s\n", link_list_new_node->str);
+		fprintf(stdout, "%s\n", link_list_new_node->str);
 
-		link_list_new = list_entry(list_head_iterator, struct linked_list, list);
-		fprintf(stderr, "%s\n", link_list_new->str);
-		fprintf(stdout, "%s\n", link_list_new->str);
-
-		free(link_list_new->str);// Free the memory.
-		free(link_list_new);// Free the memory.
+		free(link_list_new_node->str);// Free the memory.
+		free(link_list_new_node);// Free the memory.
 
 	}
 
@@ -181,16 +181,14 @@ int main(int argc, char *argv[])
 
 void freememory(char *str, struct list_head *head)
 {
-	struct list_head *list_head_iterator;
+	struct list_head *list_head_iterator, *list_head_temp;
 	// Free the memory.
 	free(str);
 
-	list_for_each(list_head_iterator, head) {
-
-		free(list_entry(list_head_iterator, struct linked_list, list)->str);
-		free(list_entry(list_head_iterator, struct linked_list, list));
+	list_for_each_safe(list_head_iterator, list_head_temp, head) {
+		free(list_entry(list_head_iterator, struct linked_list_node, list)->str);
+		free(list_entry(list_head_iterator, struct linked_list_node, list));
 
 	}
 
-	exit(1);
 }
